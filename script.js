21d180ba2ec9b26d0555267afd5e7e1979e529bf
dfussell1 <daltonfussell1@gmail.com>
2024-02-04 13:46:42 -0600

    added function to retrieve coords for city from API

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const apiKey = '9aed6d7911ae6ff9006f7346bc3c6632';
+
+document.getElementById('searchBtn').addEventListener('click', function() {
+    const city = document.getElementById('searchCity').value
+    getLongLat(city);
+});
+
+function getLongLat(city) {
+    const LongLatURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+
+    fetch(LongLatURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            // console.log(data)
+            const latitude = data[0].lat;
+            const longitude = data[0].lon;
+            console.log(latitude, longitude);
+        })
+}
